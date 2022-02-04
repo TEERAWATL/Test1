9d67e444d348354a5d562debf41202d75c754868
TEERAWATL <teerawatman0@gmail.com>
2022-02-04 16:32:27 +0700

    Create test001.py
    test

---
+++ test001.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print("-------------A Shop-------------")
+print("--------------------------------")
+print("Hawaiian pizza       x 1 : 120 B")
+print("Seafood Spaghetti    x 1 : 100 B")
+print("Potato mash          x 1 : 80  B")
+print("--------------------------------")
+print("Total                    : 300 B")
+
+
+# In[ ]:
+
+
+
+
